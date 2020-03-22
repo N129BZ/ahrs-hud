3dff731905992315020063a4a24261a689b10360
Brian Manlove <N129BZ@outlook.com>
2020-03-21 23:40:09 -0400

    Reformatted for irregular tab widths

--- public/js/hud-ops.js
+++ public/js/hud-ops.js
@@ -8,93 +8,93 @@ var host;
 var websock;
 
 try {
-	let host = "ws://" + location.hostname + ":9696";
- 	let websock = new WebSocket(host);
+  let host = "ws://" + location.hostname + ":9696";
+  let websock = new WebSocket(host);
 
-	websock.onopen = openSocket;
-	websock.onmessage = onSerialData;
+  websock.onopen = openSocket;
+  websock.onmessage = onSerialData;
 }
 catch(error) {
-	console.log(error);
+  console.log(error);
 }
 
 function openSocket() {
-	//console.log("Websocket OPENED!");
+  //console.log("Websocket OPENED!");
 }
 
 (function($) {
-	function AttitudeIndicator( placeholder, options ) {
-		var settings = $.extend({
-			size : 600,
-			roll : 0,
-			pitch : 0,
-			showBox : true,
-		}, options );
+  function AttitudeIndicator( placeholder, options ) {
+    var settings = $.extend({
+      size : 600,
+      roll : 0,
+      pitch : 0,
+      showBox : true,
+    }, options );
 
-		var constants = {
-			pitch_bound:90
-		}
+    var constants = {
+      pitch_bound:90
+    }
 
-		placeholder.each(function(){
-			$(this).html('<div class="instrument attitude"><div class="roll box"><img src="img/horizon_back.svg" class="box" alt="" /><div class="pitch box"><img src="img/horizon_ball.svg" class="box" alt="" /></div><img src="img/horizon_circle.svg" class="box" alt="" /></div><div class="mechanics box"><img src="img/horizon_airplane.svg" class="box" alt="" /></div></div>');
-			_setRoll(settings.roll);
-			_setPitch(settings.pitch);
+    placeholder.each(function(){
+      $(this).html('<div class="instrument attitude"><div class="roll box"><img src="img/horizon_back.svg" class="box" alt="" /><div class="pitch box"><img src="img/horizon_ball.svg" class="box" alt="" /></div><img src="img/horizon_circle.svg" class="box" alt="" /></div><div class="mechanics box"><img src="img/horizon_airplane.svg" class="box" alt="" /></div></div>');
+      _setRoll(settings.roll);
+      _setPitch(settings.pitch);
 
-			$(this).find('div.instrument').css({height : 500, width : 600}); 
-			$(this).find('div.instrument img.box.background').toggle(settings.showBox);
-		});
+      $(this).find('div.instrument').css({height : 500, width : 600}); 
+      $(this).find('div.instrument img.box.background').toggle(settings.showBox);
+    });
 
-		function _setRoll(roll){
-			placeholder.each(function(){
-				$(this).find('div.instrument.attitude div.roll').css('transform', 'rotate('+roll+'deg)');
-			});
-		}
+    function _setRoll(roll){
+      placeholder.each(function(){
+        $(this).find('div.instrument.attitude div.roll').css('transform', 'rotate('+roll+'deg)');
+      });
+    }
 
-		function _setPitch(pitch){
-			if(pitch>constants.pitch_bound){pitch = constants.pitch_bound;}
-			else if(pitch<-constants.pitch_bound){pitch = -constants.pitch_bound;}
-			placeholder.each(function(){
-				$(this).find('div.instrument.attitude div.roll div.pitch').css('top', pitch*0.7 + '%');
-			});
-		}
+    function _setPitch(pitch){
+      if(pitch>constants.pitch_bound){pitch = constants.pitch_bound;}
+      else if(pitch<-constants.pitch_bound){pitch = -constants.pitch_bound;}
+      placeholder.each(function(){
+        $(this).find('div.instrument.attitude div.roll div.pitch').css('top', pitch*0.7 + '%');
+      });
+    }
 
-		function _resize(size){
-			placeholder.each(function(){
-				$(this).find('div.instrument').css({height : size, width : size});
-			});
-		}
+    function _resize(size){
+      placeholder.each(function(){
+        $(this).find('div.instrument').css({height : size, width : size});
+      });
+    }
 
-		function _showBox(){
-			placeholder.each(function(){
-				$(this).find('img.box.background').show();
-			});
-		}
+    function _showBox(){
+      placeholder.each(function(){
+        $(this).find('img.box.background').show();
+      });
+    }
 
-		function _hideBox(){
-			placeholder.each(function(){
-				$(this).find('img.box.background').hide();
-			});
-		}
+    function _hideBox(){
+      placeholder.each(function(){
+        $(this).find('img.box.background').hide();
+      });
+    }
 
-		this.setRoll = function(roll){_setRoll(roll);}
-		this.setPitch = function(pitch){_setPitch(pitch);}
-		this.resize = function(size){_resize(size);}
-		this.showBox = function(){_showBox();}
-		this.hideBox = function(){_hideBox();}
+    this.setRoll = function(roll){_setRoll(roll);}
+    this.setPitch = function(pitch){_setPitch(pitch);}
+    this.resize = function(size){_resize(size);}
+    this.showBox = function(){_showBox();}
+    this.hideBox = function(){_hideBox();}
 
-		return attitude;
-	};
+    return attitude;
+  };
 
-	$.attitudeIndicator = function(placeholder, options){
-		var attitudeIndicator = new AttitudeIndicator($(placeholder), options)
-		return attitudeIndicator;
-	}
+  $.attitudeIndicator = function(placeholder, options){
+    var attitudeIndicator = new AttitudeIndicator($(placeholder), options)
+    return attitudeIndicator;
+  }
 
-	$.fn.attitudeIndicator = function(options){
-		return this.each(function(){
-			$.attitudeIndicator(this, options);
-		});
-	}
+  $.fn.attitudeIndicator = function(options){
+    return this.each(function(){
+      $.attitudeIndicator(this, options);
+    });
+  }
 }( jQuery ));
 
 $(document).keyup(function(e) {
@@ -136,14 +136,13 @@ function pad(num, size) {
 }
 
 
+//////////////////////////////////////////////////////////////////////////////
+  // Extract data from Dynon serial data stream (string)
+  //---------------------------------------------------------------------------
+  // example data:
+  // !1121144703-014+00003310811+01736+003-03+1013-033+110831245+01650023176C
+  //////////////////////////////////////////////////////////////////////////////
 function onSerialData(e) {
-
-    //////////////////////////////////////////////////////////////////////////////
-    // Extract data from Dynon serial data stream (string)
-    //---------------------------------------------------------------------------
-    // example data:
-    // !1121144703-014+00003310811+01736+003-03+1013-033+110831245+01650023176C
-    //////////////////////////////////////////////////////////////////////////////
     var str = new String(e.data);
     var roll = (parseInt(str.substr(15,5))/10);
     var pitch = (parseInt(str.substr(11,4))/10);
@@ -157,8 +156,8 @@ function onSerialData(e) {
     var TAS = parseInt(str.substr(52,4));
     var baro = parseInt(str.substr(56,3));
     var dAlt = parseInt(str.substr(59,6));
-	var winddirection = parseInt(str.substr(65,3));
-	var windkts = parseInt(str.substr(68,2));
+    var winddirection = parseInt(str.substr(65,3));
+    var windkts = parseInt(str.substr(68,2));
 
     attitude.setRoll(roll * -1);
     attitude.setPitch(pitch * pitch_offset);
@@ -174,8 +173,8 @@ function onSerialData(e) {
     arrowbox.textContent = (vertspeed < 0 ? "▼" : "▲");
     oat.textContent = "oat " + OAT + " c";
     tas.textContent = "tas " + TAS + " kt";
-	dalt.textContent = "d  alt " + dAlt + " ft";
-	
+    dalt.textContent = "d  alt " + dAlt + " ft";
+  
 
     var speedticks = (airspeed * spd_offset);
     var altticks = (altitude * alt_offset);
@@ -197,16 +196,16 @@ function onSerialData(e) {
     }
     var ballposition = ball_center + (slipskid * ball_offset);
     //console.log("slipskid: " + slipskid + ", ball position: " + ballposition)
-	ball.css('left', ballposition + 'px');
-	
-	// set the wind speed & direction
-	// fix for backwards arrow 
-	if (winddirection < 180) {
-		winddirection += 180;
-	}
-	else {
-		winddirection -= 180;	
-	} 
-	windarrow.style.transform  = 'rotate('+ winddirection +'deg)';
-	windspeed.textContent = "  " + windkts;
+    ball.css('left', ballposition + 'px');
+  
+    // set the wind speed & direction
+    // fix for backwards arrow 
+    if (winddirection < 180) {
+        winddirection += 180;
+    }
+    else {
+        winddirection -= 180; 
+    } 
+    windarrow.style.transform  = 'rotate('+ winddirection +'deg)';
+    windspeed.textContent = "  " + windkts;
 }
